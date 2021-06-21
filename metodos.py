a06eee70e1e394c9075ff39f15f708b5a0e2292e
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-06-21 02:34:45 -0300

    Commit sobre a segunda parte de métodos da aula 267. Foi ensinado sobre unpacking e packing em metodos e sobre as funções __str__ e __iter__.

--- metodos.py
+++ metodos.py
@@ -8,6 +8,13 @@ class Usuario:
     def __init__(self, email, senha):
         self.email = email
         self.senha = senha
+        self.enderecos = []
+
+    def __str__(self):
+        return f'Email: {self.email}, Senha: {self.senha}'
+
+    def __iter__(self):
+        return self.enderecos.__iter__()
 
     def _validar(self):
         email_cadastro = 'gh@gmail.com'
@@ -27,10 +34,25 @@ class Usuario:
         else:
             return False
 
+    # def cadastrar_endereco(self, endereco1, endereco2):
+    #     print(f'Endereços: {endereco1}, {endereco2}')
+    def cadastrar_endereco(self, *enderecos):
+        for endereco in enderecos:
+            print(f'Endereços: {endereco}')
+
 
 usuario = Usuario('gh@gmail.com', '1234567')
-#usuario.logar()
-if usuario.forca_senha():
-    print('Senha Forte')
-else:
-    print('Senha Fraca')
+usuario.enderecos = ['Rua 1', 'Rua 2', 'Rua 3']
+
+for endereco in usuario:
+    print(f'Endereço: {endereco}')
+
+# #usuario.logar()
+# if usuario.forca_senha():
+#     print('Senha Forte')
+# else:
+#     print('Senha Fraca')
+
+# lista = ["Rua1", "Rua2"]
+# usuario.cadastrar_endereco(*lista)
+#usuario.cadastrar_endereco('Rua 1', 'Rua 2', 'Rua 3')
